21db71e8668716a1df16139c59b57cb69f3dc360
stomp <150149808+stomp666@users.noreply.github.com>
2024-09-19 01:34:05 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+def priority(operation):
+    if operation == '+' or operation == '-':
+        return 1
+    if operation == '*' or operation == '/':
+        return 2
+    return 0
+
+def in2post(expression):
+
+    expression = expression.replace(" ", "") # remove spaces for proper calculation
+    
+    output = [] # stack 4 output
+    operators = [] # stack 4 operators (more like array but whatever)
+    
+    for char in expression: # reading expression and working with it
+
+        if char.isalnum(): # num spotted
+            output.append(char)
+        elif char == '(': # bracket open
+            operators.append(char)
+        elif char == ')':  # bracket close
+            while operators and operators[-1] != '(': # removing operations from stack till '('
+                output.append(operators.pop()) # removed operator added to output
+            operators.pop() 
+        else:  
+            while (operators and priority(operators[-1]) >= priority(char)): # checking operators for priority (current + last)
+                output.append(operators.pop())
+            operators.append(char)
+            output.append(' ') # dlya chitabelnosti
+
+    while operators: # ya ustal na english pisat commenti no UTF-8 ne daet na russkom
+        output.append(operators.pop()) # esli operatori eshe est' to ih znacheniya udalyam i dobavlyam v stack vivoda
+    
+    return ''.join(output) # bez join vigladit kak massiv
+
+if __name__ == "__main__":
+    expression = "(10 + 2) * 2"
+    postfix_expression = in2post(expression)
+    print(f"Reverse Polish Notation of '{expression}' is {postfix_expression}")
